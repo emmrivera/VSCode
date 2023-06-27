0da6ece49baadb176bd364aecd1d0951e88c6b32
emmrivera <emmrivera4@gmail.com>
2023-06-26 19:34:50 -0700

    Java & C++ sample files

--- C++/HelloWorld.cpp
+++ C++/HelloWorld.cpp
@@ -1,9 +1,15 @@
-#include <iostream>
+#include <iostream>         // For input and output functions
+#include <cstdlib>          // For random numbers
+#include <cmath>            // For math functions
+#include <string>   
+
 using namespace std;
 
 int main() {
 
-    cout << "Hello World!";
+    long num = 8000000000000;
+
+    cout << num << endl;
 
     return 0;
-}
+}   
--- SampleProject.java/src/App.java
+++ SampleProject.java/src/App.java
@@ -1,12 +1,8 @@
 public class App {
     static int[] array = {1, 2, 3, 4, 5};
 
-    public static int getNumAt(int[] array, int position) {
-        return array[position];
-    }
-
     public static void main(String[] args) throws Exception {
-        int result = getNumAt(array, 2);
-        System.out.println(result);
+
+        System.out.println("Hello World!");
     }
 }
